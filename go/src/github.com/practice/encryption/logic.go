3d52a0e4207b257cd6f0ac3dfc6748b368ab779c
mizuguchi_ef <mizuguchi@engineerforce.io>
2024-10-01 13:34:06 +0900

    fix

---
+++ go/src/github.com/practice/encryption/logic.go
@@ -0,0 +1,21 @@
+package encryption
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"os"
+)
+
+func logic() {
+	// コンソールから入力を取得
+	fmt.Println("Enter your text")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	// ハッシュ値を計算
+	hash := sha256.Sum256([]byte(input))
+
+	// ハッシュ値を表示
+	fmt.Printf("Hash: %x\n", hash)
+}
--- go/src/github.com/practice/main.go
+++ go/src/github.com/practice/main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"crypto/sha256"
-	"fmt"
-	"os"
+	"github.com/practice/encryption"
 )
 
 func main() {
-	// コンソールから入力を取得
-	fmt.Println("Enter your text")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-
-	// ハッシュ値を計算
-	hash := sha256.Sum256([]byte(input))
-
-	// ハッシュ値を表示
-	fmt.Printf("Hash: %x\n", hash)
+	encryption.Logic()
 }
